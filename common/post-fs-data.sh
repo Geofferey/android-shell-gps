91bb4fbd1c5497a44e9bebd8232c87af3adb58d7
geofferey <geofferey.eakins@gmail.com>
2019-06-18 04:48:01 +0000

    Add files

---
+++ common/post-fs-data.sh
@@ -0,0 +1,18 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+
+# This script will be executed in post-fs-data mode
+
+MGKIMG=/sbin/.core/img
+MODID=com.geofferey.gpsshell
+MODDIR=${0%/*}
+
+#pm install -r $MODDIR/gpsassist.apk
+
+ln -fs $MGKIMG/$MODID/gps-shell /sbin/gps-shell
+
+exit 0
